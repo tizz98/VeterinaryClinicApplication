1af942bdf473891b825bff8e2c03fcb219956793
tizz98 <dev.tizz98@gmail.com>
2016-01-14 13:24:36 -0800

    Pet Records

--- VeterinaryClinicApplication/frmPetRecords.Designer.vb
+++ VeterinaryClinicApplication/frmPetRecords.Designer.vb
@@ -22,17 +22,227 @@ Partial Class frmPetRecords
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblPetName = New System.Windows.Forms.Label()
+        Me.txtPetName = New System.Windows.Forms.TextBox()
+        Me.txtOwnerName = New System.Windows.Forms.TextBox()
+        Me.lblOwnerName = New System.Windows.Forms.Label()
+        Me.txtSpecies = New System.Windows.Forms.TextBox()
+        Me.lblSpecies = New System.Windows.Forms.Label()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.lblAge = New System.Windows.Forms.Label()
+        Me.lblAgeUnits = New System.Windows.Forms.Label()
+        Me.lstAgeUnits = New System.Windows.Forms.ListBox()
+        Me.chkIsNeutered = New System.Windows.Forms.CheckBox()
+        Me.txtLastVisit = New System.Windows.Forms.TextBox()
+        Me.lblLastVisit = New System.Windows.Forms.Label()
+        Me.btnGoLeft = New System.Windows.Forms.Button()
+        Me.btnNewRecord = New System.Windows.Forms.Button()
+        Me.btnGoRight = New System.Windows.Forms.Button()
+        Me.btnSaveRecord = New System.Windows.Forms.Button()
+        Me.btnCancelNewRecord = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Form1
+        'lblPetName
+        '
+        Me.lblPetName.AutoSize = True
+        Me.lblPetName.Location = New System.Drawing.Point(12, 37)
+        Me.lblPetName.Name = "lblPetName"
+        Me.lblPetName.Size = New System.Drawing.Size(57, 13)
+        Me.lblPetName.TabIndex = 0
+        Me.lblPetName.Text = "Pet Name:"
+        '
+        'txtPetName
+        '
+        Me.txtPetName.Location = New System.Drawing.Point(75, 34)
+        Me.txtPetName.Name = "txtPetName"
+        Me.txtPetName.Size = New System.Drawing.Size(231, 20)
+        Me.txtPetName.TabIndex = 1
+        '
+        'txtOwnerName
+        '
+        Me.txtOwnerName.Location = New System.Drawing.Point(474, 34)
+        Me.txtOwnerName.Name = "txtOwnerName"
+        Me.txtOwnerName.Size = New System.Drawing.Size(255, 20)
+        Me.txtOwnerName.TabIndex = 3
+        '
+        'lblOwnerName
+        '
+        Me.lblOwnerName.AutoSize = True
+        Me.lblOwnerName.Location = New System.Drawing.Point(396, 37)
+        Me.lblOwnerName.Name = "lblOwnerName"
+        Me.lblOwnerName.Size = New System.Drawing.Size(72, 13)
+        Me.lblOwnerName.TabIndex = 2
+        Me.lblOwnerName.Text = "Owner Name:"
+        '
+        'txtSpecies
+        '
+        Me.txtSpecies.Location = New System.Drawing.Point(75, 117)
+        Me.txtSpecies.Name = "txtSpecies"
+        Me.txtSpecies.Size = New System.Drawing.Size(131, 20)
+        Me.txtSpecies.TabIndex = 5
+        '
+        'lblSpecies
+        '
+        Me.lblSpecies.AutoSize = True
+        Me.lblSpecies.Location = New System.Drawing.Point(21, 120)
+        Me.lblSpecies.Name = "lblSpecies"
+        Me.lblSpecies.Size = New System.Drawing.Size(48, 13)
+        Me.lblSpecies.TabIndex = 4
+        Me.lblSpecies.Text = "Species:"
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(331, 117)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(131, 20)
+        Me.txtAge.TabIndex = 7
+        '
+        'lblAge
+        '
+        Me.lblAge.AutoSize = True
+        Me.lblAge.Location = New System.Drawing.Point(277, 120)
+        Me.lblAge.Name = "lblAge"
+        Me.lblAge.Size = New System.Drawing.Size(29, 13)
+        Me.lblAge.TabIndex = 6
+        Me.lblAge.Text = "Age:"
+        '
+        'lblAgeUnits
+        '
+        Me.lblAgeUnits.AutoSize = True
+        Me.lblAgeUnits.Location = New System.Drawing.Point(521, 120)
+        Me.lblAgeUnits.Name = "lblAgeUnits"
+        Me.lblAgeUnits.Size = New System.Drawing.Size(56, 13)
+        Me.lblAgeUnits.TabIndex = 8
+        Me.lblAgeUnits.Text = "Age Units:"
+        '
+        'lstAgeUnits
+        '
+        Me.lstAgeUnits.FormattingEnabled = True
+        Me.lstAgeUnits.Location = New System.Drawing.Point(583, 117)
+        Me.lstAgeUnits.Name = "lstAgeUnits"
+        Me.lstAgeUnits.Size = New System.Drawing.Size(120, 30)
+        Me.lstAgeUnits.TabIndex = 9
+        '
+        'chkIsNeutered
+        '
+        Me.chkIsNeutered.AutoSize = True
+        Me.chkIsNeutered.Location = New System.Drawing.Point(24, 209)
+        Me.chkIsNeutered.Name = "chkIsNeutered"
+        Me.chkIsNeutered.Size = New System.Drawing.Size(117, 17)
+        Me.chkIsNeutered.TabIndex = 11
+        Me.chkIsNeutered.Text = "Spayed/Neutered?"
+        Me.chkIsNeutered.UseVisualStyleBackColor = True
+        '
+        'txtLastVisit
+        '
+        Me.txtLastVisit.Location = New System.Drawing.Point(356, 207)
+        Me.txtLastVisit.Name = "txtLastVisit"
+        Me.txtLastVisit.Size = New System.Drawing.Size(231, 20)
+        Me.txtLastVisit.TabIndex = 13
+        '
+        'lblLastVisit
+        '
+        Me.lblLastVisit.AutoSize = True
+        Me.lblLastVisit.Location = New System.Drawing.Point(293, 210)
+        Me.lblLastVisit.Name = "lblLastVisit"
+        Me.lblLastVisit.Size = New System.Drawing.Size(52, 13)
+        Me.lblLastVisit.TabIndex = 12
+        Me.lblLastVisit.Text = "Last Visit:"
+        '
+        'btnGoLeft
+        '
+        Me.btnGoLeft.Location = New System.Drawing.Point(40, 290)
+        Me.btnGoLeft.Name = "btnGoLeft"
+        Me.btnGoLeft.Size = New System.Drawing.Size(117, 40)
+        Me.btnGoLeft.TabIndex = 14
+        Me.btnGoLeft.Text = "<<"
+        Me.btnGoLeft.UseVisualStyleBackColor = True
+        '
+        'btnNewRecord
+        '
+        Me.btnNewRecord.Location = New System.Drawing.Point(213, 290)
+        Me.btnNewRecord.Name = "btnNewRecord"
+        Me.btnNewRecord.Size = New System.Drawing.Size(334, 40)
+        Me.btnNewRecord.TabIndex = 15
+        Me.btnNewRecord.Text = "Add New Pet Record"
+        Me.btnNewRecord.UseVisualStyleBackColor = True
+        '
+        'btnGoRight
+        '
+        Me.btnGoRight.Location = New System.Drawing.Point(599, 290)
+        Me.btnGoRight.Name = "btnGoRight"
+        Me.btnGoRight.Size = New System.Drawing.Size(117, 40)
+        Me.btnGoRight.TabIndex = 16
+        Me.btnGoRight.Text = ">>"
+        Me.btnGoRight.UseVisualStyleBackColor = True
+        '
+        'btnSaveRecord
+        '
+        Me.btnSaveRecord.Location = New System.Drawing.Point(40, 290)
+        Me.btnSaveRecord.Name = "btnSaveRecord"
+        Me.btnSaveRecord.Size = New System.Drawing.Size(117, 40)
+        Me.btnSaveRecord.TabIndex = 17
+        Me.btnSaveRecord.Text = "Save"
+        Me.btnSaveRecord.UseVisualStyleBackColor = True
+        Me.btnSaveRecord.Visible = False
+        '
+        'btnCancelNewRecord
+        '
+        Me.btnCancelNewRecord.Location = New System.Drawing.Point(599, 290)
+        Me.btnCancelNewRecord.Name = "btnCancelNewRecord"
+        Me.btnCancelNewRecord.Size = New System.Drawing.Size(130, 40)
+        Me.btnCancelNewRecord.TabIndex = 18
+        Me.btnCancelNewRecord.Text = "Cancel"
+        Me.btnCancelNewRecord.UseVisualStyleBackColor = True
+        Me.btnCancelNewRecord.Visible = False
+        '
+        'frmPetRecords
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(906, 464)
-        Me.Name = "Form1"
+        Me.ClientSize = New System.Drawing.Size(761, 351)
+        Me.Controls.Add(Me.btnCancelNewRecord)
+        Me.Controls.Add(Me.btnSaveRecord)
+        Me.Controls.Add(Me.btnGoRight)
+        Me.Controls.Add(Me.btnNewRecord)
+        Me.Controls.Add(Me.btnGoLeft)
+        Me.Controls.Add(Me.txtLastVisit)
+        Me.Controls.Add(Me.lblLastVisit)
+        Me.Controls.Add(Me.chkIsNeutered)
+        Me.Controls.Add(Me.lstAgeUnits)
+        Me.Controls.Add(Me.lblAgeUnits)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.lblAge)
+        Me.Controls.Add(Me.txtSpecies)
+        Me.Controls.Add(Me.lblSpecies)
+        Me.Controls.Add(Me.txtOwnerName)
+        Me.Controls.Add(Me.lblOwnerName)
+        Me.Controls.Add(Me.txtPetName)
+        Me.Controls.Add(Me.lblPetName)
+        Me.MaximizeBox = False
+        Me.Name = "frmPetRecords"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblPetName As Label
+    Friend WithEvents txtPetName As TextBox
+    Friend WithEvents txtOwnerName As TextBox
+    Friend WithEvents lblOwnerName As Label
+    Friend WithEvents txtSpecies As TextBox
+    Friend WithEvents lblSpecies As Label
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents lblAge As Label
+    Friend WithEvents lblAgeUnits As Label
+    Friend WithEvents lstAgeUnits As ListBox
+    Friend WithEvents chkIsNeutered As CheckBox
+    Friend WithEvents txtLastVisit As TextBox
+    Friend WithEvents lblLastVisit As Label
+    Friend WithEvents btnGoLeft As Button
+    Friend WithEvents btnNewRecord As Button
+    Friend WithEvents btnGoRight As Button
+    Friend WithEvents btnSaveRecord As Button
+    Friend WithEvents btnCancelNewRecord As Button
 End Class
--- VeterinaryClinicApplication/frmPetRecords.vb
+++ VeterinaryClinicApplication/frmPetRecords.vb
@@ -1,3 +1,191 @@
-﻿Public Class frmPetRecords
+﻿Imports System.IO
+Imports System.Xml
 
+Public Class frmPetRecords
+    Private ageUnits As New List(Of String)(New String() {"Months", "Years"})
+    Private isNewRecord As Boolean
+    Private currentPetRecordIndex As Integer = 0
+    Private petRecords As New List(Of PetRecord)
+    Private Const DATAFILEPATH = "pet_records.xml"
+    Private Const FORMBASETITLE = "Veterinary Clinic System"
+
+    Private Sub frmPetRecords_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstAgeUnits.DataSource = ageUnits
+
+        If File.Exists(DATAFILEPATH) Then
+            petRecords = getRecordsFromDataFile()
+        Else
+            populateDataFile(petRecords)  ' petRecords will by an empty List
+        End If
+
+        If petRecords.Count = 0 Then
+            updateFormForNewRecord()
+        Else
+            updateFormData()
+            updateFormTitlePetCount()
+        End If
+    End Sub
+
+    Private Sub populateDataFile(records As List(Of PetRecord))
+        Dim dataFileWriter As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(DATAFILEPATH, False)
+        Dim serializer As New Serialization.XmlSerializer(GetType(List(Of PetRecord)))
+        serializer.Serialize(dataFileWriter, records)
+        dataFileWriter.Close()
+    End Sub
+
+    Private Function getRecordsFromDataFile() As List(Of PetRecord)
+        Dim serializer As New Serialization.XmlSerializer(GetType(List(Of PetRecord)))
+        Dim dataFileReader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(DATAFILEPATH)
+        Dim records As List(Of PetRecord) = DirectCast(serializer.Deserialize(dataFileReader), List(Of PetRecord))
+
+        dataFileReader.Close()
+        Return records
+    End Function
+
+    Private Function getPetStringForTitle() As String
+        Return String.Format("Pet {0}/{1}", currentPetRecordIndex + 1, petRecords.Count)
+    End Function
+
+    Private Sub updateFormTitlePetCount()
+        Dim titleString As String = String.Format("{0} - {1}", FORMBASETITLE, getPetStringForTitle())
+        Me.Text = titleString
+    End Sub
+
+    Private Sub updateFormTitleAddNewPet()
+        Dim titleString As String = String.Format("{0} - Adding New Pet", FORMBASETITLE)
+        Me.Text = titleString
+    End Sub
+
+    Private Sub btnNewRecord_Click(sender As Object, e As EventArgs) Handles btnNewRecord.Click
+        updateFormForNewRecord()
+    End Sub
+
+    Private Sub resetAllFields()
+        txtPetName.Text = ""
+        txtOwnerName.Text = ""
+        txtLastVisit.Text = ""
+        txtAge.Text = ""
+        txtSpecies.Text = ""
+        chkIsNeutered.Checked = False
+        lstAgeUnits.ClearSelected()
+    End Sub
+
+    Private Sub lockAllFields()
+        txtPetName.Enabled = False
+        txtOwnerName.Enabled = False
+        txtLastVisit.Enabled = False
+        txtAge.Enabled = False
+        txtSpecies.Enabled = False
+        chkIsNeutered.Enabled = False
+        lstAgeUnits.Enabled = False
+    End Sub
+
+    Private Sub unlockAllFields()
+        txtPetName.Enabled = True
+        txtOwnerName.Enabled = True
+        txtLastVisit.Enabled = True
+        txtAge.Enabled = True
+        txtSpecies.Enabled = True
+        chkIsNeutered.Enabled = True
+        lstAgeUnits.Enabled = True
+    End Sub
+
+    Private Sub toggleButtonsForNewRecord()
+        btnGoLeft.Hide()
+        btnGoRight.Hide()
+        btnNewRecord.Hide()
+
+        btnSaveRecord.Show()
+        btnCancelNewRecord.Show()
+    End Sub
+
+    Private Sub toggleButtonsForExistingRecord()
+        btnGoLeft.Show()
+        btnGoRight.Show()
+        btnNewRecord.Show()
+
+        btnSaveRecord.Hide()
+        btnCancelNewRecord.Hide()
+    End Sub
+
+    Private Sub updateFormForNewRecord()
+        resetAllFields()
+        unlockAllFields()
+        updateFormTitleAddNewPet()
+        toggleButtonsForNewRecord()
+    End Sub
+
+    Private Sub updateFormData()
+        lockAllFields()
+        Dim pet As PetRecord = petRecords.ElementAt(currentPetRecordIndex)
+        updateFormFieldsFromPetRecord(pet)
+    End Sub
+
+    Private Sub updateFormFieldsFromPetRecord(record As PetRecord)
+        txtPetName.Text = record.name
+        txtOwnerName.Text = record.ownerName
+        txtLastVisit.Text = record.lastVisit
+        txtAge.Text = record.age
+        txtSpecies.Text = record.species
+        chkIsNeutered.Checked = record.isNeutered
+        lstAgeUnits.SelectedIndex = ageUnits.IndexOf(record.ageUnit)
+    End Sub
+
+    Private Sub btnSaveRecord_Click(sender As Object, e As EventArgs) Handles btnSaveRecord.Click
+        ' Create record in memory & then save records to file
+        Dim record As PetRecord = getPetRecordFromFormFields()
+        petRecords.Add(record)
+        currentPetRecordIndex = petRecords.Count - 1  ' 0 based index for the List
+        updateFormData()  ' show new data
+        updateFormTitlePetCount()
+        toggleButtonsForExistingRecord()
+        populateDataFile(petRecords)
+    End Sub
+
+    Private Sub btnCancelNewRecord_Click(sender As Object, e As EventArgs) Handles btnCancelNewRecord.Click
+        If petRecords.Count = 0 Then
+            MessageBox.Show("There aren't any existing pet records, please create one first!")
+        Else
+            updateFormData()
+            updateFormTitlePetCount()
+            toggleButtonsForExistingRecord()
+        End If
+    End Sub
+
+    Function getPetRecordFromFormFields() As PetRecord
+        Dim record = New PetRecord()
+        record.name = txtPetName.Text
+        record.age = txtAge.Text
+        record.ownerName = txtOwnerName.Text
+        record.species = txtSpecies.Text
+        record.lastVisit = txtLastVisit.Text
+        record.isNeutered = chkIsNeutered.Checked
+        record.ageUnit = ageUnits.ElementAt(lstAgeUnits.SelectedIndex)
+
+        Return record
+    End Function
+
+    Private Sub btnGoLeft_Click(sender As Object, e As EventArgs) Handles btnGoLeft.Click
+        Dim newIndex = currentPetRecordIndex - 1
+
+        If newIndex < 0 Then
+            MessageBox.Show("You cannot move past the first record!")
+        Else
+            currentPetRecordIndex = newIndex
+            updateFormData()
+            updateFormTitlePetCount()
+        End If
+    End Sub
+
+    Private Sub btnGoRight_Click(sender As Object, e As EventArgs) Handles btnGoRight.Click
+        Dim newIndex = currentPetRecordIndex + 1
+
+        If newIndex > (petRecords.Count - 1) Then
+            MessageBox.Show("You cannot move past the last record!")
+        Else
+            currentPetRecordIndex = newIndex
+            updateFormData()
+            updateFormTitlePetCount()
+        End If
+    End Sub
 End Class
