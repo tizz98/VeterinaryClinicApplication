0115a131cd7f0611bda168797a537cf45c0f271e
tizz98 <dev.tizz98@gmail.com>
2016-01-14 13:33:48 -0800

    PetRecord
    PetRecord class

---
+++ VeterinaryClinicApplication/PetRecord.vb
@@ -0,0 +1,9 @@
+﻿<System.Serializable> Public Class PetRecord
+    Public name As String
+    Public ownerName As String
+    Public species As String
+    Public age As String
+    Public ageUnit As String
+    Public isNeutered As Boolean
+    Public lastVisit As String
+End Class
